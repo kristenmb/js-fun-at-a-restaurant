3be6dd429a396b786156d8bb86599f4695c0d1e8
Kristen Bair <kristenmbair@gmail.com>
2020-10-08 19:05:25 -0600

    Complete meal.js testing

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
+};
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type,
+  };
+  return menuItem;
+};
+
+function addIngredients(item1, ingredients) {
+  if (item1 != ingredients[0] && item1 != ingredients[1]) {
+    return ingredients.push(item1);
+  } else {
+    return;
+  };
+};
+//but how do i set this up for an unknown length of array?
+
+
+function formatPrice(price) {
+  return "$" + price;
+};
+
+function decreasePrice(originalPrice) {
+  return originalPrice - (originalPrice / 10);
 }
 
+function createRecipe(name, ingredients, type) {
+  return recipe = {
+    title: name,
+    ingredients: ingredients,
+    type: type,
+  };
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
