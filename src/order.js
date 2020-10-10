aa07d3282a8b568a14150f35c1b186a1f44f7ed4
Kristen Bair <kristenmbair@gmail.com>
2020-10-09 20:31:38 -0600

    Complete order.js test

--- src/order.js
+++ src/order.js
@@ -1,7 +1,42 @@
+function takeOrder(order, deliveryOrders) {
+  if(deliveryOrders.length < 3) {
+    return deliveryOrders.push(order);
+  } else {
+    return;
+  };
+}
+
+function refundOrder(refundRequest, deliveryOrders) {
+    deliveryOrders.splice((refundRequest - 1), 1);
+    return deliveryOrders;
+};
+
+function listItems(deliveryOrders) {
+  var itemName = '';
+  for (var i = 0; i < deliveryOrders.length; i++) {
+   var itemNameList = `${deliveryOrders[i].item}`;
+   if(i < deliveryOrders.length - 1) {
+     itemNameList += ', '
+   }
+  itemName += itemNameList
+ };
+ return itemName;
+};
+
+function searchOrder(deliveryOrders, itemInput) {
+  var isPresent = false;
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === itemInput) {
+      isPresent = true;
+    };
+  };
+  return isPresent;
+};
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
