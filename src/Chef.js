e14bf3e8ffcebcaab093b942321d646100d38640
Kristen Bair <kristenmbair@gmail.com>
2020-10-11 17:53:42 -0600

    Complete Chef.js tests

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,30 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
   }
+
+  greetCustomer(name, time) {
+    if(time === true) {
+      return `Good morning, ${name}!`
+    } else if(time === false) {
+      return `Hello, ${name}!`
+    } else {
+    return `Hello, ${name}!`;
+  };
+};
+
+  checkForFood(foodItem) {
+    if(this.restaurant.menus.breakfast.includes(foodItem) === true) {
+      return `Yes, we're serving ${foodItem.name} today!`;
+    } else if(this.restaurant.menus.lunch.includes(foodItem) === true) {
+      return `Yes, we're serving ${foodItem.name} today!`;
+    } else if(this.restaurant.menus.dinner.includes(foodItem) === true) {
+      return `Yes, we're serving ${foodItem.name} today!`;
+    } else {
+      return `Sorry, we aren't serving ${foodItem.name} today.`;
+  }
+};
 }
 
 module.exports = Chef;
